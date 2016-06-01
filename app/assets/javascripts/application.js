efdb07e445e6a0f76e23bdda59e230bf207f23ef
Andre Fagerberg <andre.fagerberg@lodon.se>
2016-06-01 23:35:11 +0200

    JQuery scroll function, updated about me section and title

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,3 +14,35 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
+
+
+ready = function() {
+
+  var hashTagActive = "";
+  $(".scroll").click(function (event) {
+    if(hashTagActive != this.hash) {
+      event.preventDefault();
+
+        var dest = 0;
+        if ($(this.hash).offset().top > $(document).height() - $(window).height()) {
+          dest = $(document).height() - $(window).height();
+        } else {
+          dest = $(this.hash).offset().top;
+        }
+        //go to destination
+        $('html,body').animate({
+          scrollTop: dest
+        }, 1500, 'swing');
+        hashTagActive = this.hash;
+      }
+  });
+
+};
+
+$(document).ready(ready);
+$(document).on('page:load', ready);
+
+
+
+
+
